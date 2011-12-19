a2a92d82379221374822a3147b2c9b6fd72be1d1
Jason Smith (air) <jhs@iriscouch.com>
2011-12-19 12:43:18 +0700

    Refactor validation tests to the beginning

--- test/reduce.js
+++ test/reduce.js
@@ -8,23 +8,6 @@ var tap = require('tap')
 
 var stew = require('../views/lib/stew')
 
-test('Defining reducers', function(t) {
-  var vals = [ { 'name':'Bob', 'people':2, 'pencils':1  }
-             , { 'name':'Joe', 'people':5               }
-             , { 'name':'Ann', 'people':3, 'pencils':15 }
-             ]
-
-  var reducer = stew.reducer('people', 'pencils')
-    , result = reducer(ks(vals), vals, false)
-
-  t.equal(result.count  , 3 , 'Three rows emitted')
-  t.equal(result.people , 10, 'Ten total people')
-  t.equal(result.pencils, 16, "Sixteen pencils, Joe's missing value counts as 0")
-  t.equal(Object.keys(result).length, 3, 'Only keys for people, pencils, and count')
-
-  t.end()
-})
-
 test('Reducer input validation', function(t) {
   var vals = [ { 'n': 1 }
              , { 'n': 2 }
@@ -54,6 +37,23 @@ test('Reducer input validation', function(t) {
   t.throws(function() { stew.reduce(keys, vals, false, true )}, 'Throw for boolean key name')
   t.throws(function() { stew.reduce(keys, vals, false, ['x'])}, 'Throw for array key name')
   t.throws(function() { stew.reduce(keys, vals, false, {k:5})}, 'Throw for object key name')
+
+  t.end()
+})
+
+test('Defining reducers', function(t) {
+  var vals = [ { 'name':'Bob', 'people':2, 'pencils':1  }
+             , { 'name':'Joe', 'people':5               }
+             , { 'name':'Ann', 'people':3, 'pencils':15 }
+             ]
+
+  var reducer = stew.reducer('people', 'pencils')
+    , result = reducer(ks(vals), vals, false)
+
+  t.equal(result.count  , 3 , 'Three rows emitted')
+  t.equal(result.people , 10, 'Ten total people')
+  t.equal(result.pencils, 16, "Sixteen pencils, Joe's missing value counts as 0")
+  t.equal(Object.keys(result).length, 3, 'Only keys for people, pencils, and count')
 
   t.end()
 })
