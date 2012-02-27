dddd2b6300131708059c13abfc16a612c1c4cf56
Jason Smith (work) <jhs@iriscouch.com>
2012-02-27 08:27:27 +0000

    The basic test app

--- test_kanso/test.sh
+++ test_kanso/test.sh
@@ -5,5 +5,10 @@ PATH="../node_modules/.bin:$PATH"
 couch="http://localhost:5984"
 db="$couch/stew_test"
 
+curl -X DELETE "$db"
 kanso upload --force docs "$db"
 kanso push . "$db"
+
+echo
+set -x
+curl -i "$db/_design/stew_test/_view/fruit?group_level=1"
